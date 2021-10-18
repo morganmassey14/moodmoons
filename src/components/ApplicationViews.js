762ae7811b319d1c8f5eed2da8f6e0a16b419b40
tgann11 <morganne1170@gmail.com>
2021-10-18 11:45:04 -0500

    feat(moodmoons-4):added login/logout/register

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -3,13 +3,24 @@ import { Route } from "react-router-dom"
 import { Journal } from "./journal/JournalList"
 import { YogaPoseList } from "./yoga/YogaPoseList"
 import { MeditationList } from "./meditations/MeditationList"
+import { Login } from "./auth/Login"
+import { Register } from "./auth/Register"
+import { Redirect } from "react-router"
 
 
-export const ApplicationViews = () => {
+export const ApplicationViews = ({ isAuthenticated, setAuthUser }) => {
     return (
         <>
             <Route exact path="/">
-                <Journal />
+                {isAuthenticated ? <Journal /> : <Redirect to="/login" />}
+            </Route>
+
+            <Route exact path="/login">
+                <Login setAuthUser={setAuthUser} />
+            </Route>
+
+            <Route path="/register">
+                <Register setAuthUser={setAuthUser} />
             </Route>
 
             <Route exact path="/yogaposes">
--- src/components/MoodMoons.js
+++ src/components/MoodMoons.js
@@ -1,12 +1,28 @@
-import React from "react"
+import React, {useState} from "react"
 import { ApplicationViews } from "./ApplicationViews"
 import { NavBar } from "./nav/NavBar"
 
 export const Moodmoons = () => {
+    
+
+  const [isAuthenticated, setIsAuthenticated] = useState(sessionStorage.getItem("moodmoons_user") !== null)
+
+  const setAuthUser = (user) => {
+    sessionStorage.setItem("moodmoons_user", user.id)
+    sessionStorage.setItem("moodmoons_username", user.name)
+    setIsAuthenticated(sessionStorage.getItem("moodmoons_user") !== null)
+  }
+
+  const clearUser = () => {
+    sessionStorage.clear();
+    setIsAuthenticated(sessionStorage.getItem("moodmoons_user") !== null)
+  }
+
+    
     return (
         <>
-        <NavBar />
-        <ApplicationViews />
+        <NavBar clearUser={clearUser} isAuthenticated={isAuthenticated} />
+        <ApplicationViews setAuthUser={setAuthUser} isAuthenticated={isAuthenticated} />
         </>
     )
 }
