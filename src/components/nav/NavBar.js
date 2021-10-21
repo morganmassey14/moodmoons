ce862c34e6487021e45bb1ac3b33d1b208c88b45
tgann11 <morganne1170@gmail.com>
2021-10-21 15:57:11 -0500

    added styling to login and register

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -26,32 +26,26 @@ export const NavBar = ({ clearUser, isAuthenticated }) => {
                 <div className="logo__img"><img className="logo" src={logo} alt="Mood Moons" /></div>
                 <div className="nav__flex__bar">
                 <ul className="navbar">
-                    {isAuthenticated ?
+            
+                    
                         <li className="navbar__link">
                             <Link className="navbar__link" to="/">Journal</Link>
                         </li>
-                        : null}
-                    {isAuthenticated ?
+                
                         <li className="navbar__link">
                             <Link className="navbar__link" to="/yogaposes">Yoga</Link>
                         </li>
-                        : null}
-                    {isAuthenticated ?
                         <li className="navbar__link">
                             <Link className="navbar__link" to="/meditations">Meditations</Link>
                         </li>
-                        : null}
-                    {isAuthenticated ?
+                    
                         <li className="navbar__link">
                             <Link className="navbar__link" to="/breathingexercises">Breathing</Link>
                         </li>
-                        : null}
-
-                    {isAuthenticated ?
+                
                         <li className="navbar__link" >
                             <a className="navbar__link" onClick={handleLogout} >Logout</a>
                         </li>
-                        : null}
                 </ul>
                 </div>
             </nav>
